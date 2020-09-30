f41f3c43e8630600531665b84a942a54b094db24
Riti Mishra <ritimishra02@gmail.com>
2020-09-30 11:02:03 +0530

    dotnet

---
+++ Hello World/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Hello_World
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.lb1HelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(133, 88);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(75, 23);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "Click this";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.btnClickThis_Click);
+            // 
+            // lb1HelloWorld
+            // 
+            this.lb1HelloWorld.AutoSize = true;
+            this.lb1HelloWorld.Location = new System.Drawing.Point(140, 143);
+            this.lb1HelloWorld.Name = "lb1HelloWorld";
+            this.lb1HelloWorld.Size = new System.Drawing.Size(35, 13);
+            this.lb1HelloWorld.TabIndex = 1;
+            this.lb1HelloWorld.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lb1HelloWorld);
+            this.Controls.Add(this.btnClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label lb1HelloWorld;
+    }
+}
+
